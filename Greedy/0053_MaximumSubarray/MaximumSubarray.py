6b63e9d4a8d68f456f8d9cd645fbf510868201b3
Alex Ryan <alexryan1717@hotmail.com>
2022-06-29 20:47:45 -0500

    added solution description

--- Greedy/0053_MaximumSubarray/MaximumSubarray.py
+++ Greedy/0053_MaximumSubarray/MaximumSubarray.py
@@ -1,3 +1,7 @@
+# For this problem, we want to keep track of the max sum that we have found so far, and see if adding the current
+# number increases that. This solution iterates through the input array, and keeps track of the max sum found so far.
+# If it ever goes negative, it is reset to zero. We keep track of the max so far by taking the max of the max that we
+# have found previously and the current max.
 class Solution:
 	def maxSubArray(self, nums: List[int]) -> int:
 		maxSub = nums[0]
